ccb71df108656e72f15e4d3f02c3ba78254efe96
Jérémy <jeremyvdw@gmail.com>
2011-12-15 09:56:15 +0100

    Added basic methods SimpleReactor::Base#next_tick, SimpleReactor::Base#tick and SimpleReactor::Base#run

--- lib/simple_reactor/base.rb
+++ lib/simple_reactor/base.rb
@@ -7,8 +7,41 @@ class SimpleReactor::Base
   end
 
   def add_timer(time, *args, &block)
-    time = time.to_i if Time === time
     @timers.add_timer time, *args, &block
+  end
+
+  def next_tick(&block)
+    @block_buffer << block
+  end
+
+  def stop
+    @running = false
+  end
+
+  def run
+    @running = true
+
+    yield self if block_given?
+
+    tick while running
+  end
+
+  def tick
+    handle_pending_block
+    handle_events
+    handle_timers
+  end
+
+  def handle_pending_block
+    @block_buffer.size.times { @block_buffer.shift.call }
+  end
+
+  def handle_events
+  end
+
+  def handle_timers
+    now = Time.now.to_i
+    @timers.call_next_timer while !@timers.empty? && @timers.next_time < now
   end
 
 end
--- spec/simple_reactor/base_spec.rb
+++ spec/simple_reactor/base_spec.rb
@@ -1,18 +1,108 @@
 require 'spec_helper'
-require 'pry'
 
 describe SimpleReactor::Base do
-
-  it "#initialize set defaults attributes" do
-    subject.instance_variable_get(:@running).should be_false
-    subject.instance_variable_get(:@timers).should be_kind_of SimpleReactor::TimerMap
-    subject.instance_variable_get(:@timers).empty?.should be_true
-    subject.instance_variable_get(:@block_buffer).should eq []
+  let(:proc) { Proc.new { "Hi from the block!"} }
+  
+  describe "#initialize" do
+    it " set defaults attributes" do
+      subject.instance_variable_get(:@running).should be_false
+      subject.instance_variable_get(:@timers).should be_kind_of SimpleReactor::TimerMap
+      subject.instance_variable_get(:@timers).empty?.should be_true
+      subject.instance_variable_get(:@block_buffer).should eq []
+    end
   end
 
-  it "#add_timer pushes given timer to TimerMap#add_timer" do
-    time, args, proc = Time.now + 5, 5, Proc.new { |arg| "Hi #{arg}!" }
-    subject.instance_variable_get(:@timers).should_receive(:add_timer).with(time.to_i, args, &proc)
-    subject.add_timer time, args, &proc
+  describe "#add_timer" do
+    it "pushes given timer to TimerMap#add_timer" do
+      time, args, proc = Time.now + 5, 5, Proc.new { |arg| "Hi #{arg}!" }
+      subject.instance_variable_get(:@timers).should_receive(:add_timer).with(time, args, &proc)
+      subject.add_timer time, args, &proc
+    end
+  end
+
+  describe "#next_tick" do
+    it "append a new proc to the block_buffer" do
+      subject.next_tick &proc
+      subject.instance_variable_get(:@block_buffer).size.should eq 1
+      subject.instance_variable_get(:@block_buffer).should eq [proc]
+    end
+  end
+
+  describe "#stop" do
+    it "stops the reactor"
+  end
+
+  describe "#tick" do
+    it "call the next proc shifted from the block_buffer" do
+      subject.should_receive(:handle_pending_block)
+    end
+    
+    it "call for incomming event" do
+      subject.should_receive(:handle_events)
+    end
+    
+    it "call scheduled tasks" do
+      subject.should_receive(:handle_timers)
+    end
+    
+    after { subject.tick }
+  end
+
+  describe "#handle_pending_block" do
+    context "when @block_buffer is empty" do
+      it "returns zero" do
+        subject.handle_pending_block.should be 0
+      end
+    end
+
+    context "when @block_buffer contains 1 element" do
+      it "calls the proc in block_buffer heap" do
+        subject.next_tick &proc
+        subject.instance_variable_get(:@block_buffer).first.should_receive(:call)
+        subject.handle_pending_block
+      end
+    end
+
+    context "when @block_buffer contains many elements" do
+      it "calls all procs in block_buffer heap" do
+        3.times { subject.next_tick &proc }
+        subject.instance_variable_get(:@block_buffer).each { |buffer| buffer.should_receive(:call) }
+        subject.handle_pending_block
+        subject.instance_variable_get(:@block_buffer).should be_empty
+      end
+    end
+
+  end
+
+  describe "#handle_events" do
+    it "does nothing for now"
+  end
+
+  describe "#handle_timers" do
+    context "when no timers has been set up" do
+      it "returns 0" do
+        subject.handle_timers.should be_nil
+      end
+    end
+
+    context "when a timer has been set up" do
+      it "calls timer on time" do
+        now = Time.now - 1
+        proc.should_receive(:call).and_return(true)
+        subject.add_timer now, nil, &proc
+        subject.handle_timers
+      end
+
+      it "doesn't call timer before time" do
+        now = Time.now + 5
+        proc.should_not_receive(:call).and_return(true)
+        subject.add_timer now, nil, &proc
+        subject.handle_timers
+      end
+    end
+  end
+
+  describe "#run" do
+    it "starts the reactor"
   end
 end
